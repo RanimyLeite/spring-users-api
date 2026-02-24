1a9dc1a33dce1572afabab4ae68585801e2e6fa8
Ranimy Leite <ranimy.leitecosta@gmail.com>
2026-02-24 09:37:28 -0300

    feat: creating BaseEntity

---
+++ src/main/java/com/example/springusersapi/domain/entity/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.example.springusersapi.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createDate;
+
+    @Column()
+    private LocalDateTime updateDate;
+
+    @Column()
+    private LocalDateTime deleteDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updateDate = LocalDateTime.now();
+    }
+}
